139b52241cfb58f7ed2776d1b88c0691bb91b407
alviroepsa <alviro@epsa.upv.es>
2014-05-14 23:59:50 +0200

    Version 1.0 newImageRect

---
+++ newImageTutorial/main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+local background = display.newImage("images/background.jpg",display.contentWidth/2,display.contentHeight/2)
